be993177d85d3d08af845c4ac6de220ccec21029
nghochik@gmail.com <nghochik@gmail.com@e0822149-dd3e-f7ff-2650-29aab4592713>
2011-11-17 17:45:43 +0000

    BorrowDirect: service dataDamp report filename

--- grails-app/controllers/metridoc/penn/bd/ServiceController.groovy
+++ grails-app/controllers/metridoc/penn/bd/ServiceController.groovy
@@ -6,30 +6,15 @@ class ServiceController {
 	def borrowDirectService
 	
 	private static final DATE_FORMAT = "yyyyMMdd";
-    def systemWide = { 
-		def dateFrom = DateUtil.getDate(params.dateFrom, DATE_FORMAT)
-		def dateTo = DateUtil.getDate(params.dateTo, DATE_FORMAT)
-		if(dateFrom != null && dateTo != null){
-			dateTo = DateUtil.getDateEndOfDay(dateTo);
-			def itemTimes = params.int("itemTimes");
-			if(itemTimes == null){
-				itemTimes = 1
-			}
-						
-			response.setHeader("Content-Disposition", "attachment;filename=\"multiple_items.xlsx\"");
-			response.setContentType("application/vnd.ms-excel")
-			borrowDirectService.dumpDataMultipleItems(dateFrom, dateTo, itemTimes, response.outputStream, params.serviceKey)
-		}else{
-			render("Incorrect dates");
-		}
-	}
 	
 	def dataDump = {
 		def library_id = params.int("libraryId")
 		def dateFrom = DateUtil.getDate(params.dateFrom, DATE_FORMAT)
 		def dateTo = DateUtil.getDate(params.dateTo, DATE_FORMAT)
 		if(dateFrom != null && dateTo != null && library_id != null){
-			response.setHeader("Content-Disposition", "attachment;filename=\"my_library_dump.xlsx\"");
+			def library = borrowDirectService.getLibraryById(params.serviceKey, library_id)
+			def libname = library != null?library.institution:""
+			response.setHeader("Content-Disposition", "attachment;filename=\""+libname+"_data_dump_"+params.dateFrom+"-"+params.dateTo+".xlsx\"");
 			response.setContentType("application/vnd.ms-excel")
 			borrowDirectService.dumpDataLibrary(library_id, dateFrom, dateTo, response.outputStream, params.serviceKey)
 		}else{
