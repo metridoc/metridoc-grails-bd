434c7902edb08f78f870bc5409f77c5ffca923a4
Tommy Barker <mingus.karate@gmail.com>
2013-07-10 13:58:20 -0400

    remove unused files

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,22 +1,10 @@
-// locations to search for config files that get merged into the main config
-// config files can either be Java properties files or ConfigSlurper scripts
-
-// grails.config.locations = [ "classpath:${appName}-config.properties",
-//                             "classpath:${appName}-config.groovy",
-//                             "file:${userHome}/.grails/${appName}-config.properties",
-//                             "file:${userHome}/.grails/${appName}-config.groovy"]
 
 grails.config.locations = [ Queries,
 	                             "file:${userHome}/.grails/BdDataSource.groovy",
 								 "file:${userHome}/.grails/BdPasswords.groovy"]
 	
-	
-// if(System.properties["${appName}.config.location"]) {
-//    grails.config.locations << "file:" + System.properties["${appName}.config.location"]
-// }
-
-grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
-grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
+grails.project.groupId = appName
+grails.mime.file.extensions = true
 grails.mime.use.accept.header = false
 grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
                       xml: ['text/xml', 'application/xml'],
@@ -33,31 +21,19 @@ grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
 					  excel: 'application/vnd.ms-excel'*/
                     ]
 
-// URL Mapping Cache Max Size, defaults to 5000
-//grails.urlmapping.cache.maxsize = 1000
-
-// The default codec used to encode data with ${}
 grails.views.default.codec = "html"//"none" // none, html, base64
 grails.views.gsp.encoding = "UTF-8"
 grails.converters.encoding = "UTF-8"
-// enable Sitemesh preprocessing of GSP pages
 grails.views.gsp.sitemesh.preprocess = true
-// scaffolding templates configuration
 grails.scaffolding.templates.domainSuffix = 'Instance'
 
-// Set to false to use the new Grails 1.2 JSONBuilder in the render method
 grails.json.legacy.builder = false
-// enabled native2ascii conversion of i18n properties files
 grails.enable.native2ascii = true
-// whether to install the java.util.logging bridge for sl4j. Disable for AppEngine!
 grails.logging.jul.usebridge = true
-// packages to include in Spring bean scanning
 grails.spring.bean.packages = []
 
-// request parameters to mask when logging exceptions
 grails.exceptionresolver.params.exclude = ['password']
 
-// set per-environment serverURL stem for creating absolute links
 environments {
     production {
         grails.serverURL = "http://www.changeme.com"
@@ -73,24 +49,20 @@ environments {
 datafarm.title.ext = " - BETA"
 datafarm.minFiscalYear=2011
 datafarm.minCalYear=2010
-// log4j configuration
 log4j = {
-    // Example of changing the log pattern for the default console
-    // appender:
-    //
     appenders {
         console name:'stdout', layout:pattern(conversionPattern: '%d %c{2} %m%n')
         'null' name:'stacktrace'
     }
 
-    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
-           'org.codehaus.groovy.grails.web.pages', //  GSP
-           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
-           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
-           'org.codehaus.groovy.grails.web.mapping', // URL mapping
-           'org.codehaus.groovy.grails.commons', // core / classloading
-           'org.codehaus.groovy.grails.plugins', // plugins
-           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+    error  'org.codehaus.groovy.grails.web.servlet',
+           'org.codehaus.groovy.grails.web.pages',
+           'org.codehaus.groovy.grails.web.sitemesh',
+           'org.codehaus.groovy.grails.web.mapping.filter',
+           'org.codehaus.groovy.grails.web.mapping',
+           'org.codehaus.groovy.grails.commons',
+           'org.codehaus.groovy.grails.plugins',
+           'org.codehaus.groovy.grails.orm.hibernate',
            'org.springframework',
            'org.hibernate',
            'net.sf.ehcache.hibernate'
