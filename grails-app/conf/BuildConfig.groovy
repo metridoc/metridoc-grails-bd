f8facd22f179151cd343d370b87661c248da8398
Tommy Barker <mingus.karate@gmail.com>
2013-07-30 09:41:08 -0400

    set target and source level 1.6
    this ensures compatibility with java 6
    this is especially important since
    our build server uses 1.7.  Also
    changed the application version
    to 0.9.1 since these changes are related to
    that

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,6 +1,9 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
+grails.project.source.level = 1.6
+
 grails.project.dependency.resolution = {
     inherits("global")
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
